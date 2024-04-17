5bc03eb532b1aa68cd909ec98c1898f5e2ab62e9
rahafibrahim21 <rahafibrahim999@gmail.com>
2024-04-17 09:54:56 +0300

    Initial Commit

---
+++ Macro-rahaf/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Macro-rahaf
+//
+//  Created by Rahaf ALghuraibi on 08/10/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Macro-rahaf/Macro_rahafApp.swift
@@ -0,0 +1,17 @@
+//
+//  Macro_rahafApp.swift
+//  Macro-rahaf
+//
+//  Created by Rahaf ALghuraibi on 08/10/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct Macro_rahafApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
