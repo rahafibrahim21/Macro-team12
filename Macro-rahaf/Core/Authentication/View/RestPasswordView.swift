2e486ac3f5caa7d71a4434cec03ad5387b0d7e6c
rahafibrahim21 <rahafibrahim999@gmail.com>
2024-04-25 09:52:28 +0300

    10

---
+++ Macro-rahaf/Core/Authentication/View/RestPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  RestPasswordView.swift
+//  Macro-rahaf
+//
+//  Created by Rahaf ALghuraibi on 14/10/1445 AH.
+//
+
+import SwiftUI
+
+struct RestPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RestPasswordView()
+}
